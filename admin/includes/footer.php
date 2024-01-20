c3d0fd5ae81cf8b3ad6afe3bc9662adce07ed7d4
WisdomAkai <31257592+Wisdomakai@users.noreply.github.com>
2024-01-20 11:58:59 +0000

    Delete admin directory

--- admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>All rights reserved</b>
-    </div>
-    <bold>Copyright &copy; 2020 <a href="https://vbitconsult.com/"> VB IT CONSULT</a></bold>
-</footer>
